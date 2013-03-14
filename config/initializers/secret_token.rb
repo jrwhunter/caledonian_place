6d44e7cecbdbf8059cd9a5337984078c4a7891ba
jrwhunter <j.hunter@abdn.ac.uk>
2013-03-14 13:36:34 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CaledonianPlace::Application.config.secret_token = '437253af55cb0e626dc6daa28223fecbeb68bfc443e7ea9494c236ef0123f59f0d7234e5b6b548b88e4a42cfb1722112f85a07f52bf2be2d531ea01dcd77c3bf'
