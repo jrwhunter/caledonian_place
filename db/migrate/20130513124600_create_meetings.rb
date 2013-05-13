a58e3f33289fc84d599a8aacc68041af4a42bc6f
jrwhunter <j.hunter@abdn.ac.uk>
2013-05-13 20:12:13 +0100

    Meetings migrate

---
+++ db/migrate/20130513124600_create_meetings.rb
@@ -0,0 +1,13 @@
+class CreateMeetings < ActiveRecord::Migration
+  def change
+    create_table :meetings. :force=>true do |t|
+      t.date :date
+      t.string :venue
+      t.string :author
+      t.string :title
+
+      t.timestamps
+    end
+  end
+end
+
