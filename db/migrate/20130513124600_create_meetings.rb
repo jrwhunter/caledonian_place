ee3f85bb68ee59045781260bdd4fef74241f919d
jrwhunter <j.hunter@abdn.ac.uk>
2013-05-13 20:17:56 +0100

    Meetings migrate bug corrected

--- db/migrate/20130513124600_create_meetings.rb
+++ db/migrate/20130513124600_create_meetings.rb
@@ -1,6 +1,6 @@
 class CreateMeetings < ActiveRecord::Migration
   def change
-    create_table :meetings. :force=>true do |t|
+    create_table :meetings do |t|
       t.date :date
       t.string :venue
       t.string :author
